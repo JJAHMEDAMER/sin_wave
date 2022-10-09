281ef87a01abe2895e34ae831c8696754beef122
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-10-09 18:40:01 +0200

    draw sine wave

--- sketch.js
+++ sketch.js
@@ -1,8 +1,24 @@
+t = 0;
+
 function setup() {
   createCanvas(400, 400);
+  background(0);
+  stroke(255);
+  strokeWeight(5);
 }
 
 function draw() {
-  background(220);
+    point(x(t), y(t) +200);
+    t++;
 }
 
+
+// parametric equations
+
+function x(t){
+  return t
+}
+
+function y(t){
+  return sin(t/20) * -150
+}
